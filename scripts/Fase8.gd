f0ab27caac0c68e39fe9a62bc7af55e754f82542
Gustaviusss <henrique.g@acad.ifma.edu.br>
2020-10-04 23:42:43 -0300

    update1.2

---
+++ scripts/Fase8.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+func _ready():
+	pass
+
+
+func _on_b1_body_entered(body):
+	$anim.play("1_move")
+
+func _on_b2_body_entered(body):
+	$anim.play("2_move")
+
+
+func _on_b4_body_entered(body):
+	$anim.play("4_move")
+
+
+func _on_b3_body_entered(body):
+	$anim.play("3_move")
+
+
+func _on_b5_body_entered(body):
+	get_tree().reload_current_scene()
